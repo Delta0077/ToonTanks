f7380a174a49a66f51ceea6943d453f8e0d6c6f1
Delta0077 <adas0262@gmail.com>
2021-03-09 21:05:59 +0530

    Update HealthComponent.cpp

--- Source/ToonTanks/Components/HealthComponent.cpp
+++ Source/ToonTanks/Components/HealthComponent.cpp
@@ -22,14 +22,28 @@ void UHealthComponent::BeginPlay()
 
 	Health = DefaultHealth;
 	GameModeRef = Cast<ATankGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage);
 
 }
 
 void UHealthComponent::TakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser) 
 {
-	
+	if(Damage == 0)
+	{
+		return;
+	}
+
+	Health = FMath::Clamp(Health - Damage, 0.0f, DefaultHealth);
+
+	if(Health <= 0)
+	{
+		if(GameModeRef)
+		{
+			GameModeRef->ActorDied(GetOwner());
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Health Component has no reference to the Game Mode! "))
+		}
+	}
 }
-
-
-
-
